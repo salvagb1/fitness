6875d2dbead4a78c4c44d8e5955dd632ec3d3ff9
salvagb1 <salvadorgb06@gmail.com>
2022-04-12 18:17:17 -0500

    Update camera_btn

--- lib/pages/navigatorbtn/camera_btn.dart
+++ lib/pages/navigatorbtn/camera_btn.dart
@@ -1,3 +1,4 @@
+import 'package:fitness/components/button.dart';
 import 'package:fitness/components/card_gallery.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -40,6 +41,97 @@ class _CameraBtnState extends State<CameraBtn> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: const <BoxShadow>[
+                  BoxShadow(
+                    color: Color(0xffF7F8F8),
+                    blurRadius: 10.0,
+                  ),
+                ],
+                color: const Color(0xffE2E7FF),
+              ),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Track Your Progress Each',
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      const Text(
+                        'Month With Photo',
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          margin: const EdgeInsets.only(),
+                          height: 40,
+                          width: 105,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color(0xff9DCEFF),
+                                Color(0xff92A3FD),
+                              ],
+                            ),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              'Leanr More',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [Image.asset('assets/images/calendar2.png')],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              padding: const EdgeInsets.all(18),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: const <BoxShadow>[
+                  BoxShadow(
+                    color: Color(0xffF7F8F8),
+                    blurRadius: 10.0,
+                  ),
+                ],
+                color: const Color(0xffE2E7FF),
+              ),
+              child: Row(
+                children: const [
+                  Expanded(
+                    child: Text(
+                      "Compare my photo",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                  ButtonReu(textbu: 'textbu'),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 35,
+            ),
             Row(
               children: [
                 Expanded(
